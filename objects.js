b4b766173f048789dd2aaf5566da89f9eae438e5
Vik Vangala <vvang044@gmail.com>
2019-05-28 18:05:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,10 @@
 const playlist = {michael: 'thriller', elvis: 'something'};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   return playlist
+}
+
+function removeFrommPlayList(playlist,artistName){
+  delete playlist[artistName]
 }
